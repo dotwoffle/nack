1e17e0e63bc7004345e0a374e8806b72a6164568
dotwoffle <54816222+dotwoffle@users.noreply.github.com>
2026-07-24 12:15:38 -0600

    Added token struct

--- interpreter/src/parsing/lexing.rs
+++ interpreter/src/parsing/lexing.rs
@@ -1,6 +1,23 @@
-pub fn tokenize_source_string(source_string: &str) -> Vec<String> {
-    source_string
-        .split(" ")
-        .map(str::to_owned)
-        .collect::<Vec<String>>()
+#[derive(Debug)]
+pub struct Token {
+    position: SourcePosition,
+    value: String,
+    kind: TokenKind,
+}
+
+pub fn tokenize_source_string(source_string: &str) -> Vec<Token> {
+    vec![]
+}
+
+#[derive(Debug)]
+struct SourcePosition {
+    line: u64,
+    column: u64,
+}
+
+#[derive(Debug)]
+enum TokenKind {
+    Eof,
+    Identifier,
+    Ignore,
 }
