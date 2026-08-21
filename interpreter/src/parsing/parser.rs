6dd36f82f3d34a508ffe6902869d8dbc21ad37e7
dotwoffle <lorenztanner71@gmail.com>
2026-08-21 13:23:21 -0600

    Fixed debug print of AST

--- interpreter/src/parsing/parser.rs
+++ interpreter/src/parsing/parser.rs
@@ -43,7 +43,7 @@ impl ASTNode {
 
     /// Prints this node's debug string representation, then all of its children indented below it.
     fn dump(&self, indent: usize, fmt: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
+        writeln!(
             fmt,
             "{}{}",
             "  ".repeat(indent),
@@ -156,7 +156,7 @@ mod parser_tests {
                 value: String::from("true"),
                 kind: TokenKind::Identifier,
             }])
-            .handle_expr_atom_rule()?,
+                .handle_expr_atom_rule()?,
             ASTNode::of_token(
                 Token {
                     position: SourcePosition { line: 0, column: 0 },
@@ -172,7 +172,7 @@ mod parser_tests {
                 value: String::from("123"),
                 kind: TokenKind::IntLiteral,
             }])
-            .handle_expr_atom_rule()?,
+                .handle_expr_atom_rule()?,
             ASTNode::of_token(
                 Token {
                     position: SourcePosition { line: 0, column: 0 },
@@ -188,7 +188,7 @@ mod parser_tests {
                 value: String::from("foo"),
                 kind: TokenKind::Identifier,
             }])
-            .handle_expr_atom_rule()?,
+                .handle_expr_atom_rule()?,
             ASTNode::of_token(
                 Token {
                     position: SourcePosition { line: 0, column: 0 },
@@ -210,8 +210,8 @@ mod parser_tests {
                 value: String::new(),
                 kind: TokenKind::Eof
             }])
-            .handle_expr_atom_rule()
-            .is_err()
+                .handle_expr_atom_rule()
+                .is_err()
         );
     }
 
@@ -223,7 +223,7 @@ mod parser_tests {
                 value: String::from("foo"),
                 kind: TokenKind::Identifier,
             }])
-            .handle_expression_rule()?,
+                .handle_expression_rule()?,
             ASTNode::of_grouping(
                 EXPRESSION_LABEL,
                 vec![ASTNode::of_token(
@@ -248,8 +248,8 @@ mod parser_tests {
                 value: String::new(),
                 kind: TokenKind::Eof
             }])
-            .handle_expression_rule()
-            .is_err()
+                .handle_expression_rule()
+                .is_err()
         );
     }
 
