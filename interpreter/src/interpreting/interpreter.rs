d09ccd445cdeb9000bcb43ed8d6ff6d2e5b3feac
dotwoffle <lorenztanner71@gmail.com>
2026-09-01 22:05:54 -0600

    Added some tests for evaluating expressions

--- interpreter/src/interpreting/interpreter.rs
+++ interpreter/src/interpreting/interpreter.rs
@@ -78,6 +78,46 @@ impl NackInterpreter {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub enum InterpreterError {
     Runtime,
+}
+
+#[cfg(test)]
+mod expression_tests {
+    use super::*;
+    use crate::lexing::{SourcePosition, Token, TokenKind};
+    use crate::parsing::parser::{ASTNode, ASTNodeType};
+
+    #[test]
+    fn test_interpreting_expr_atoms_returns_correct_values() {
+        assert_eq!(
+            NackInterpreter::new().evaluate_expression(&ASTNode {
+                node_type: ASTNodeType::Token(Token {
+                    position: SourcePosition { line: 0, column: 0 },
+                    value: String::from("123"),
+                    kind: TokenKind::IntLiteral,
+                }),
+                children: vec![],
+            }),
+            Ok(NackValue {
+                value_type: String::from("Int"),
+                value: CoreNackValue::Int(123)
+            })
+        );
+        assert_eq!(
+            NackInterpreter::new().evaluate_expression(&ASTNode {
+                node_type: ASTNodeType::Token(Token {
+                    position: SourcePosition { line: 0, column: 0 },
+                    value: String::from("true"),
+                    kind: TokenKind::Identifier,
+                }),
+                children: vec![],
+            }),
+            Ok(NackValue {
+                value_type: String::from("Bool"),
+                value: CoreNackValue::Bool(true)
+            })
+        );
+    }
 }
--- interpreter/src/interpreting/values.rs
+++ interpreter/src/interpreting/values.rs
@@ -1,9 +1,10 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum CoreNackValue {
     Int(i64),
     Bool(bool),
 }
 
+#[derive(Debug, PartialEq)]
 pub struct NackValue {
     pub value_type: String,
     pub value: CoreNackValue,
